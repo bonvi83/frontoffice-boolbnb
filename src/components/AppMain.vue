317979382610b26d435b333c0e8efee07047faa5
bonvi83 <bonvi83@gmail.com>
2024-05-08 16:36:22 +0200

    add: macro struttura main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,115 @@
-<script>
-</script>
-<template>
-    <h1>Main</h1>
-    
+<script></script>
 
+<template>
+  <section class="container">
+    <div class="mb-2">
+      <h2>Appartamenti in evidenza</h2>
+
+      <div class="d-flex">
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-2">
+      <h2>Eventualmente altri bei appartamentini</h2>
+
+      <div class="d-flex">
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+          <img
+            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+            class="card-img-top"
+            alt="immagine mancante"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Titolo casa</h5>
+            <a href="#" class="btn btn-primary">Vedi info</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
