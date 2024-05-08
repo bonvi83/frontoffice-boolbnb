a84678b9b44007f52a224dfc7ed9364f3961079d
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-05-08 11:27:34 +0200

    add scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+  components: { AppHeader, AppMain, AppFooter },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
-// import './style.css'
+import './styles/general.scss'
 import * as bootstrap from "bootstrap";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+
+const app = createApp(App);
+app.mount("#app");
