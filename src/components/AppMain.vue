e11c9089c8bf1ba8b131e541feaccacf799c2c14
lucapolzone <lucapolzone@gmail.com>
2024-05-09 16:58:19 +0200

    feat: add no sponsored apartments style

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -40,7 +40,7 @@ export default {
 </script>
 
 <template>
-  <section class="container bg-body-secondary">
+  <section class="container">
     <div class="mb-2">
       <h2>Appartamenti in evidenza</h2>
 
@@ -102,10 +102,31 @@ export default {
     </div>
 
     <div class="mb-2">
-      <h2>Eventualmente altri bei appartamentini</h2>
+      <h2 class="mt-5">Appartamenti senza sponsorizzazione</h2>
+      <div class="row g-4 mt-3">
+        <div v-for="apartment in apartments" class="col-3">
+          <div class="card h-100">
+            <a href="#">
+              <img :src="apartment.cover_img" class="card-img-top" alt="immagine mancante"/>
+            </a>
+            <div class="card-body">
+              <h5 class="card-title">
+                <a href="#">
+                  {{ apartment.name }}
+                </a>
+              </h5>
+              <p>
+                <a href="#">
+                  {{ apartment.address }}
+                </a>
+              </p>
+              <!-- <a href="#">Vedi i dettagli
+                <i class="fa-solid fa-eye"></i>
+              </a> -->
+            </div>
+          </div>
 
-      <div class="d-flex">
-        <div class="card">
+         <!-- <div class="card">
           <img
             src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
             class="card-img-top"
@@ -139,24 +160,30 @@ export default {
             <h5 class="card-title">Titolo casa</h5>
             <a href="#" class="btn btn-primary">Vedi info</a>
           </div>
-        </div>
+        </div> -->
 
-        <div class="card">
-          <img
-            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
-            class="card-img-top"
-            alt="immagine mancante"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Titolo casa</h5>
-            <a href="#" class="btn btn-primary">Vedi info</a>
-          </div>
         </div>
       </div>
+      
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+  .card-title {
+    font-weight: bold;
+  }
 
+  .card-body {
+    
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    a:hover {
+      color: #0d6efd;
+    }
+
+  }
 </style>
