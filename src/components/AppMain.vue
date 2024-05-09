7139acab51af78eec7246309c19964fd5d4c6fe5
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-05-09 17:53:17 +0200

    import component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from "axios";
-
+import AppMainHero from "./AppMainHero.vue";
 
 export default {
   data() {
@@ -9,6 +9,8 @@ export default {
       // apartment: null,
     };
   },
+
+  components: { AppMainHero },
 
   methods: {
     fetchApartments() {
@@ -40,30 +42,37 @@ export default {
 </script>
 
 <template>
-  <section class="container bg-body-secondary">
-    <div class="mb-2">
-      <h2>Appartamenti in evidenza</h2>
 
-      <div class="d-flex">
-        <div v-for="apartment in apartments.slice(0,4)" class="card">
-          <!-- <img
+
+    <AppMainHero />
+
+
+
+    <section class="container bg-body-secondary">
+
+        <div class="mb-2">
+            <h2>Appartamenti in evidenza</h2>
+
+            <div class="d-flex">
+                <div v-for="apartment in apartments.slice(0,4)" class="card">
+                    <!-- <img
             src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
             class="card-img-top"
             alt="immagine mancante"
           /> -->
 
-          <img :src="apartment.cover_img" alt="immagine-placeholder" />
+                    <img :src="apartment.cover_img" alt="immagine-placeholder" />
 
-          <div class="card-body">
-            <!-- <h5 class="card-title">Titolo casa</h5> -->
-            <h5 class="card-title">
-              {{ apartment.name }}
-            </h5>
-            <a href="#" class="btn btn-primary">Vedi info</a>
-          </div>
-        </div>
+                    <div class="card-body">
+                        <!-- <h5 class="card-title">Titolo casa</h5> -->
+                        <h5 class="card-title">
+                            {{ apartment.name }}
+                        </h5>
+                        <a href="#" class="btn btn-primary">Vedi info</a>
+                    </div>
+                </div>
 
-        <!-- <div class="card">
+                <!-- <div class="card">
           <img
             src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
             class="card-img-top"
@@ -98,63 +107,51 @@ export default {
             <a href="#" class="btn btn-primary">Vedi info</a>
           </div>
         </div> -->
-      </div>
-    </div>
-
-    <div class="mb-2">
-      <h2>Eventualmente altri bei appartamentini</h2>
-
-      <div class="d-flex">
-        <div class="card">
-          <img
-            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
-            class="card-img-top"
-            alt="immagine mancante"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Titolo casa</h5>
-            <a href="#" class="btn btn-primary">Vedi info</a>
-          </div>
+            </div>
         </div>
 
-        <div class="card">
-          <img
-            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
-            class="card-img-top"
-            alt="immagine mancante"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Titolo casa</h5>
-            <a href="#" class="btn btn-primary">Vedi info</a>
-          </div>
-        </div>
+        <div class="mb-2">
+            <h2>Eventualmente altri bei appartamentini</h2>
 
-        <div class="card">
-          <img
-            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
-            class="card-img-top"
-            alt="immagine mancante"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Titolo casa</h5>
-            <a href="#" class="btn btn-primary">Vedi info</a>
-          </div>
-        </div>
+            <div class="d-flex">
+                <div class="card">
+                    <img src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+                        class="card-img-top" alt="immagine mancante" />
+                    <div class="card-body">
+                        <h5 class="card-title">Titolo casa</h5>
+                        <a href="#" class="btn btn-primary">Vedi info</a>
+                    </div>
+                </div>
 
-        <div class="card">
-          <img
-            src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
-            class="card-img-top"
-            alt="immagine mancante"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Titolo casa</h5>
-            <a href="#" class="btn btn-primary">Vedi info</a>
-          </div>
+                <div class="card">
+                    <img src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+                        class="card-img-top" alt="immagine mancante" />
+                    <div class="card-body">
+                        <h5 class="card-title">Titolo casa</h5>
+                        <a href="#" class="btn btn-primary">Vedi info</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+                        class="card-img-top" alt="immagine mancante" />
+                    <div class="card-body">
+                        <h5 class="card-title">Titolo casa</h5>
+                        <a href="#" class="btn btn-primary">Vedi info</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="../assets/01_Loggia_degli_Osii_in_Piazza_Mercanti,_Milano_-_Foto_Giovanni_Dall'Orto,_3-gen-2007.jpeg"
+                        class="card-img-top" alt="immagine mancante" />
+                    <div class="card-body">
+                        <h5 class="card-title">Titolo casa</h5>
+                        <a href="#" class="btn btn-primary">Vedi info</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <style lang="scss" scoped>
