75c3fbb82d6b616ace04166f6d296653054bf613
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-05-10 12:30:14 +0200

    add router and create show apartment components

---
+++ src/components/AppShow.vue
@@ -0,0 +1,36 @@
+<script>
+
+import AppShowCarousel from "./show-components/AppShowCarousel.vue";
+import AppShowMessages from "./show-components/AppShowMessages.vue";
+import AppShowServices from "./show-components/AppShowServices.vue";
+
+
+export default {
+    data() {
+        return {};
+    },
+
+
+    components: { AppShowCarousel, AppShowMessages, AppShowServices }
+
+};
+</script>
+
+
+<template>
+    <div>
+        <AppShowCarousel></AppShowCarousel>
+    </div>
+
+    <div>
+        <AppShowMessages></AppShowMessages>
+    </div>
+
+    <div>
+        <AppShowServices></AppShowServices>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/show-components/AppShowServices.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+
+<template>
+    <div>
+        <h1>SERVIZI</h1>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
