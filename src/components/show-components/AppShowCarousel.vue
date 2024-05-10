ec2e1ef4b4b6cba41e56e4e82afa0fe019ed58e4
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-05-11 00:04:51 +0200

    add structure and style carousel

--- src/components/show-components/AppShowCarousel.vue
+++ src/components/show-components/AppShowCarousel.vue
@@ -1,5 +1,4 @@
 <script>
-
 // AXIOS
 import axios from "axios";
 // STORE
@@ -8,12 +7,9 @@ import { api, store } from "../../store";
 export default {
     data() {
         return {
-            store, 
-
+            store,
             coverImage: '',
-
-            images: [],
-
+            images: '',
         };
     },
 
@@ -29,10 +25,10 @@ export default {
         fetchImages(endpoint = this.apiEndpoint) {
             axios.get(endpoint).then((res) => {
 
-                if (res.data.cover_img && res.data.apartment_images ) {
+                if (res.data.cover_img && res.data.apartment_images) {
                     this.coverImage = res.data.cover_img;
                     console.log(this.coverImage)
-                    
+
                     this.images = res.data.apartment_images;
                     console.log(this.images)
                 } else {
@@ -57,11 +53,51 @@ export default {
 
 
 <template>
-    <div>
+    <section class="container">
+
         <h1>CAROSELLO</h1>
-    </div>
+
+        <div class="carousel-container row">
+
+            <!-- COVER -->
+            <div class="col-6 bordered">
+
+                <div class="h-100 d-flex justify-content-center align-items-center bordered">
+                    <img :src="coverImage" alt="" class="object-fit-cover card-img rounded bordered">
+                </div>
+
+            </div>
+
+            <!-- THUMBS -->
+            <div class="col-6 d-flex justify-content-center align-items-center bordered">
+
+
+                <div class="row justify-content-center align-items-center g-1">
+
+                    <div class="col-4 bordered thumb-container" v-for="image in images" :key="index">
+                        <div class="h-100">
+                            <img class="h-100 card-img rounded bordered" :src="image.url" alt="">
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </section>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+section {
 
+    .bordered {
+        border: solid 5px black;
+    }
+
+        
+
+}
 </style>
