2ee3a1945526ae34a9537a46b45df50b8da01255
bonvi83 <bonvi83@gmail.com>
2024-05-10 17:11:39 +0200

    add method

--- src/components/show-components/AppShowServices.vue
+++ src/components/show-components/AppShowServices.vue
@@ -3,18 +3,24 @@ export default {
   data() {
     return {};
   },
+
+  computed: {},
+
+  method: {
+    fetchApartment(apartmentId) {
+      axios.get('http://127.0.0.1:8000/api/apartments/${id_appartmentId}').then((res)) => {
+        this.apartment = res.data;
+        console.log(this.apartment);
+      };
+    },
+  },
 };
 </script>
 
 <template>
   <div>
     <h1>SERVIZI</h1>
-    <div>
-      card con tutte le iconcine dei servizi
-      <br />
-      chiama backend
-      <!-- http://127.0.0.1:8000/api/apartments/${id_appartamento} -->
-    </div>
+    <div>card con tutte le iconcine dei servizi</div>
   </div>
 </template>
 
