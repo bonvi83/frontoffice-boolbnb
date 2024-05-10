c72914b445fca91662186bb704d1f129abf19edb
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-05-10 19:00:01 +0200

    feat: add apartment images api

--- src/components/show-components/AppShowCarousel.vue
+++ src/components/show-components/AppShowCarousel.vue
@@ -1,7 +1,50 @@
 <script>
+
+// AXIOS
+import axios from "axios";
+// STORE
+// import { api, store } from "../store";
+
 export default {
     data() {
-        return {};
+        return {
+            // store, 
+
+            coverImage: '',
+
+            images: [],
+
+        };
+    },
+
+    methods: {
+
+        // API CALL FOR SERVICES
+        getImages() {
+            axios.get(`http://127.0.0.1:8000/api/apartments/${this.$route.params.id}`).then((res) => {
+
+                if (res.data.cover_img && res.data.apartment_images ) {
+                    this.coverImage = res.data.cover_img;
+                    console.log(this.coverImage)
+                    
+                    this.images = res.data.apartment_images;
+                    console.log(this.images)
+                } else {
+                    // redirect alla pagina 404
+                    // this.$router.push({ name: 'not-found' })
+
+                    console.log('not found')
+                }
+
+            });
+        },
+
+    },
+
+    created() {
+
+        this.getImages()
+
     },
 };
 </script>
