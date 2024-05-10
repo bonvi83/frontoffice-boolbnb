917a4e6b37f63a7c29cc438a3547def7a3d4d96a
bonvi83 <bonvi83@gmail.com>
2024-05-10 16:52:45 +0200

    macrostruttura AppShowServices

--- src/components/show-components/AppShowServices.vue
+++ src/components/show-components/AppShowServices.vue
@@ -1,16 +1,21 @@
 <script>
 export default {
-    data() {
-        return {};
-    },
+  data() {
+    return {};
+  },
 };
 </script>
 
 <template>
+  <div>
+    <h1>SERVIZI</h1>
     <div>
-        <h1>SERVIZI</h1>
+      card con tutte le iconcine dei servizi
+      <br />
+      chiama backend
+      <!-- http://127.0.0.1:8000/api/apartments/${id_appartamento} -->
     </div>
+  </div>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
