fab4567f5e5de6c62954eba7bd1b0b39a3cee3a7
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-05-14 16:55:00 +0200

    feat: add responsive carousel images

--- src/components/show-components/AppShowCarousel.vue
+++ src/components/show-components/AppShowCarousel.vue
@@ -67,25 +67,15 @@ export default {
 
 <template>
   <section class="container">
-    <div
-      class="carousel-container bordered row justify-content-between align-items-center"
-    >
+    <div class="carousel-container bordered row justify-content-between align-items-center flex-lg-wrap flex-xl-nowrap">
       <!-- COVER CONTAINER-->
-      <div
-        class="col-6 h-100 cover-container bordered d-flex justify-content-center align-items-center"
-      >
+      <div class="col-xl-6 col-lg-12 cover_img bordered d-flex justify-content-center align-items-center">
         <!-- COVER IMG -->
         <div class="card h-100 w-100">
-          <img
-            :src="coverImage"
-            alt=""
-            class="img-fluid d-block rounded bordered h-100"
-          />
+          <img :src="coverImage" alt="" class="cover_img img-fluid d-block rounded h-100" />
 
-          <div
-            v-if="this.coverImage != this.cover"
-            class="card-img-overlay d-flex justify-content-center align-items-end"
-          >
+          <div v-if="this.coverImage != this.cover"
+            class="card-img-overlay d-flex justify-content-center align-items-end">
             <button @click="backToCover()" class="cover-btn btn rounded">
               Back to cover
             </button>
@@ -94,16 +84,10 @@ export default {
       </div>
 
       <!-- THUMBS CONTAINER-->
-      <div
-        class="col-6 h-100 d-flex justify-content-center align-items-center bordered"
-      >
+      <div class="col-lg-12 col-xl-6 h-100 d-flex justify-content-center align-items-center mt-1 mt-xl-0 bordered">
         <div class="row h-100 justify-content-center align-items-center g-1">
           <!-- THUMBS IMGS -->
-          <div
-            @click="changeImg(index)"
-            class="col-4 thumb-container"
-            v-for="(imgUrl, index) in imgsUrls"
-          >
+          <div @click="changeImg(index)" class="col-6 col-md-4  thumb-container" v-for="(imgUrl, index) in imgsUrls">
             <div class="h-100 card">
               <img class="h-100 card-img" :src="imgUrl" alt="" />
             </div>
@@ -116,29 +100,77 @@ export default {
 
 <style lang="scss" scoped>
 section {
-  // .bordered {
-  //     border: solid 5px black;
-  // }
 
   .carousel-container {
     height: 490px;
+  }
 
-    // .cover-container {
-
-    // }
-
-    .cover-btn {
-      color: white;
-      background-color: rgba(0, 0, 0, 0.4);
+  @media screen and (min-width: 1199px) {
+    .cover_img {
+      height: 320px;
+      object-fit: cover;
     }
 
-    .thumb-container {
-      height: 160px;
+  }
 
-      .active-thumb {
-        border: 5px solid cornflowerblue;
-      }
+  @media screen and (max-width: 1199px) {
+    .carousel-container {
+      height: auto;
     }
+
+    .cover_img {
+      height: 500px;
+
+    }
+
+  }
+
+  @media screen and (max-width: 992px) {
+    .carousel-container {
+      height: auto;
+    }
+
+    .cover_img {
+      height: 400px;
+
+    }
+  }
+
+  @media screen and (max-width: 767px) {
+    .carousel-container {
+      height: auto;
+    }
+
+    .cover_img {
+      height: 300px;
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+  .cover-btn {
+    color: white;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  .thumb-container {
+    height: 150px;
+
+    .active-thumb {
+      border: 5px solid cornflowerblue;
+    }
+  }
+
+  .bordered {
+    border: 5px solid black;
   }
 }
 </style>
