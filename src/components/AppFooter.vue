f4eb86d5d177b15b8e303a20dced5ac4be02aa7c
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-05-15 11:20:15 +0200

    feat: style footer and responsive

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,60 +1,67 @@
 <script></script>
 
 <template>
-  <footer class="text-white">
-    <div
-      class="container-fluid py-4 d-flex justify-content-between align-items-center ps-4"
-    >
-      <div class="team">
-        <p class="h6">&copy; CLASS 117, TEAM 1</p>
+  <footer class="text-white d-flex justify-content-between align-items-center">
+    <div class="container-fluid w-100 d-flex justify-content-between align-items-center">
+
+      <div class="d-flex justify-content-between align-items-center">
+
+        <div class="d-none d-sm-block">
+          <router-link :to="{ name: 'home' }" class="nav-link active " aria-current="page">
+            <img src="../assets/logbnb.png" alt="Logo" class=" logo" />
+          </router-link>
+        </div>
+
+        <div class="h-100 px-0 px-sm-2 ">
+          BoolBnB &copy;, Group 1 S.r.l <br> Made with &hearts; by Group 1
+        </div>
         <!-- <p style="font-size: 10px" class="fw-lighter"></p> -->
       </div>
 
-      <div class="icon-footer">
-        <ul class="d-flex align-items-center ps-0">
-          <li>
-            <a href="https://www.facebook.com/"
-              ><i class="fa-brands fa-facebook fa-xl" style="color: #ffffff"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com"
-              ><i
-                class="fa-brands fa-instagram fa-xl"
-                style="color: #ffffff"
-              ></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com"
-              ><i class="fa-brands fa-youtube fa-xl" style="color: #ffffff"></i
-            ></a>
-          </li>
-        </ul>
+
+      <div>
+        <div class="d-none d-sm-block">
+          Seguici sui nostri social:
+        </div>
+
+        <div class="d-flex justify-content-center align-items-center">
+          <div>
+            <a href="https://www.facebook.com/">
+              <i class="fa-brands fa-facebook fa-xl" style="color: #ffffff"></i>
+            </a>
+          </div>
+
+          <div class="px-3">
+            <a href="https://www.instagram.com">
+              <i class="fa-brands fa-instagram fa-xl" style="color: #ffffff"></i>
+            </a>
+          </div>
+
+          <div>
+            <a href="https://www.youtube.com">
+              <i class="fa-brands fa-youtube fa-xl" style="color: #ffffff"></i>
+            </a>
+          </div>
+        </div>
       </div>
 
-      <div class="pe-2">
-        <a href=""
-          ><router-link
-            :to="{ name: 'home' }"
-            class="nav-link active"
-            aria-current="page"
-            ><img src="../assets/logbnb.png" alt="Logo" height="55"
-          /></router-link>
-        </a>
-      </div>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 footer {
+  min-height: 100px;
   background-color: rgb(0, 27, 54);
 }
 
-.container {
-  align-items: baseline;
-  display: flex;
+// .container {
+//   align-items: baseline;
+//   display: flex;
+// }
+
+.logo {
+  height: 70px;
 }
 
 ul {
@@ -64,5 +71,11 @@ ul {
 li {
   list-style: none;
   padding: 6px;
+}
+
+@media screen and (max-width: 576px) {
+  .logo {
+    height: 60px;
+  }
 }
 </style>
